00df962874f3da588c5bdbec5a38465431376095
Garrett Berg <Garrett@cloudformdesign.com>
2013-10-08 15:50:07 -0500

    Just taking some notes at this point

---
+++ regui_interface.py
@@ -0,0 +1,16 @@
+
+
+from __future__ import __division__
+
+
+'''
+Features:
+    - Highlights matches as light green
+    - colorizes and numbers matches like kiki
+    - Automatically updates text view as you write the re (greys out if no match)
+    - Replacements turn the re into a light red background and then add a bold, red ->
+        which points to the replacement in light yellow.
+    - Helper tab which helps construct the regular expressions 
+    - Uses file-system's indexing to get help on where to look first.
+    
+'''
