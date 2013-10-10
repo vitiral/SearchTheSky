542a0f3b6399510c219cb1220dd0c672ef7859e0
Garrett Berg <Garrett@cloudformdesign.com>
2013-10-09 22:47:43 -0500

    Gui is looking pretty good

--- run_pyuic.py
+++ run_pyuic.py
@@ -7,5 +7,5 @@ Created on Fri Aug 31 19:48:11 2012
 
 if __name__ == '__main__':
     import os
-    print os.system("pyuic4 regui_ui.ui > regui_ui.py")
+    print os.system("pyuic4 sky_ui.ui > sky_ui.py")
 print 'done'
